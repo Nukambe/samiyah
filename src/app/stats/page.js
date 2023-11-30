1257b15a0b92e7f4b1e61c7089261f959e05b67c
nukambe <chappellwesley@gmail.com>
2023-11-29 21:12:16 -0500

    stats list

--- src/app/stats/page.js
+++ src/app/stats/page.js
@@ -4,7 +4,7 @@ export default async function StatsPage() {
   const attributes = await getDocuments("attributes");
 
   return (
-    <div className="bg-gray-900 py-24 sm:py-32">
+    <div className="bg-gray-900 h-full py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
         <div className="mx-auto max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
@@ -17,24 +17,19 @@ export default async function StatsPage() {
         </div>
         <ul
           role="list"
-          className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
+          className="mt-16"
         >
           {attributes.map((attribute) => (
             <li
               key={attribute.id}
-              className="rounded-2xl bg-gray-800 px-8 py-10"
+              className=" mx-auto mt-4 flex items-center justify-between w-80"
             >
-              {/* <img
-                className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56"
-                src={person.imageUrl}
-                alt=""
-              /> */}
-              <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">
-                {attribute.value}
-              </h3>
               <p className="text-sm leading-6 text-gray-400">
                 {attribute.attribute}
               </p>
+              <h3 className="font-semibold leading-7 tracking-tight text-white">
+                {attribute.value}
+              </h3>
             </li>
           ))}
         </ul>
